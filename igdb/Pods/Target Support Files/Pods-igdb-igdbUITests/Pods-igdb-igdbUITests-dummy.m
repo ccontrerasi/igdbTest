6e607d8ddef97ae37b7d184f7c855694a003ce6b
Christian Contreras <cristian.contreras.izquierdo@gmail.com>
2023-02-08 11:56:37 +0100

    Removing pod directory

--- igdb/Pods/Target Support Files/Pods-igdb-igdbUITests/Pods-igdb-igdbUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_igdb_igdbUITests : NSObject
-@end
-@implementation PodsDummy_Pods_igdb_igdbUITests
-@end
--- igdb/Pods/Target Support Files/Pods-igdb/Pods-igdb-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_igdb : NSObject
-@end
-@implementation PodsDummy_Pods_igdb
-@end
